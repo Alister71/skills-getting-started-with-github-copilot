d0a74885e9d428ae7064e4131cebda54c36af8c3
Serhii Kushnerenko <sergeal71@gmail.com>
2025-10-01 15:39:44 +0000

    Update availability label in activity display

--- src/static/app.js
+++ src/static/app.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
           <h4>${name}</h4>
           <p>${details.description}</p>
           <p><strong>Schedule:</strong> ${details.schedule}</p>
-          <p><strong>Availability 1:</strong> ${spotsLeft} spots left</p>
+          <p><strong>Availability:</strong> ${spotsLeft} spots left</p>
           ${participantsHTML}
         `;
 
